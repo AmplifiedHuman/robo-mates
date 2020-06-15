d5eb352c2a54a2124d13b4420da722b773e6c3d4
AmplifiedHuman <amplifiedhuman@gmail.com>
2020-06-15 23:04:59 +0800

    Updates

---
+++ precache-manifest.351bf67177639dffe493972c8d8756f6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b52e06ecdb59b490798db433dc9417e",
+    "url": "/robo-mates/index.html"
+  },
+  {
+    "revision": "e14c5260784ccd9979a3",
+    "url": "/robo-mates/static/css/2.d97600f8.chunk.css"
+  },
+  {
+    "revision": "a96362496fe6807fa9f3",
+    "url": "/robo-mates/static/css/main.04c7f69c.chunk.css"
+  },
+  {
+    "revision": "e14c5260784ccd9979a3",
+    "url": "/robo-mates/static/js/2.c3813f6d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/robo-mates/static/js/2.c3813f6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a96362496fe6807fa9f3",
+    "url": "/robo-mates/static/js/main.ba24c03c.chunk.js"
+  },
+  {
+    "revision": "4ec5ed1ed308ba41f857",
+    "url": "/robo-mates/static/js/runtime-main.3bf70206.js"
+  },
+  {
+    "revision": "ecf427f7510fa5366337cefeeeb727d2",
+    "url": "/robo-mates/static/media/robot.ecf427f7.ttf"
+  }
+]);
